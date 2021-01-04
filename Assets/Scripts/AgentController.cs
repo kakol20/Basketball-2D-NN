1706b8a26be173911c554d5145978b47d974991f
Adrian <adrianaquino.aquino33@gmail.com>
2021-01-04 15:17:44 +0000

    Initial Upload
    Just a moving rectangle for now

---
+++ Assets/Scripts/AgentController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = Input.mousePosition;
+
+        newPos.z = Camera.main.nearClipPlane;
+        newPos = Camera.main.ScreenToWorldPoint(newPos);
+
+        transform.position = newPos;
+    }
+}
