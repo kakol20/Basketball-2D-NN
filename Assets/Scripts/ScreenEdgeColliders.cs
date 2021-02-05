0769e65ef652bbb642bfa900d0d72fbba5053726
Adrian <adrianaquino.aquino33@gmail.com>
2021-02-05 20:33:38 +0000

    Build v0.5.3

--- Assets/Scripts/ScreenEdgeColliders.cs
+++ Assets/Scripts/ScreenEdgeColliders.cs
@@ -37,7 +37,7 @@ namespace UnityLibrary
         {
             //Vector2's for the corners of the screen
             Vector2 bottomLeft = cam.ScreenToWorldPoint(new Vector3(0, 0, cam.nearClipPlane));
-            Vector2 topRight = cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth, cam.pixelHeight + 100f, cam.nearClipPlane));
+            Vector2 topRight = cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth, cam.pixelHeight * 2f, cam.nearClipPlane));
             Vector2 topLeft = new Vector2(bottomLeft.x, topRight.y);
             Vector2 bottomRight = new Vector2(topRight.x, bottomLeft.y);
 
