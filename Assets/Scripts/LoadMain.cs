40997fb78b74b3b8e5d3eb5c0a508657132b63ea
Adrian <adrianaquino.aquino33@gmail.com>
2021-02-07 00:36:57 +0000

    Create Menu

---
+++ Assets/Scripts/LoadMain.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadMain : MonoBehaviour
+{
+    [SerializeField] private Slider loadingBar;
+    [SerializeField] private TMP_InputField seedInput;
+    //[SerializeField] private GameObject[] toHide;
+    [SerializeField] private GameObject[] toShow;
+    public void LoadMainScene()
+    {
+        //print(seedInput.text);
+
+        if (seedInput.text.Length > 0)
+        {
+            StaticManager.Seed = int.Parse(seedInput.text);
+        }
+
+        print(StaticManager.Seed);
+
+        StartCoroutine(LoadMainSceneAsync());
+
+        for (int i = 0; i < toShow.Length; i++)
+        {
+            toShow[i].SetActive(true);
+        }
+
+        //for (int i = 0; i < toHide.Length; i++)
+        //{
+        //    toHide[i].SetActive(false);
+        //}
+    }
+
+    private IEnumerator LoadMainSceneAsync()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Main");
+
+        while (!operation.isDone)
+        {
+            // update progress
+
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            //Debug.Log(progress);
+
+            loadingBar.value = progress;
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/StaticManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticManager : MonoBehaviour
+{
+    public static int Seed { get; set; } = 1337;
+}
